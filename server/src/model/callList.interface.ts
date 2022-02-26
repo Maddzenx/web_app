250f89aa4b2f822fb40fac691589685742b9681c
salvija <salvija.zelvyte22@gmail.com>
2022-02-26 07:34:20 +0100

    added react stuff
    callList.tsx and App.tsx (everybody contributed but Jasper slept thru as always)

--- server/src/model/callList.interface.ts
+++ server/src/model/callList.interface.ts
@@ -3,7 +3,8 @@ import { User } from "./user.interface";
 
 export interface CallList {
     id: number;
+    title: string;
     creator: string;
     contacts: Array<Contact["id"]>;
-    decription: string;
+    decription?: string;
 }
