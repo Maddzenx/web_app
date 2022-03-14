7b3de5245b1a1956ccbf69558c9c9b3e6737bb15
BQ <jesper.bergquist1@gmail.com>
2022-03-14 10:32:30 +0100

    Fixed types

--- server/src/service/icallList.service.ts
+++ server/src/service/icallList.service.ts
@@ -4,9 +4,9 @@ import { Contact } from "../model/contact.interface";
 
 export interface ICallListService {
 getCallList() : Promise<Array<CallList>>
-getOneCallList(id:any) : Promise<CallList>
+getOneCallList(id: number) : Promise<CallList>
 createCallList(title: string, creator: string, decription: string) : Promise<CallList>
-editCallList(id:number, title:string) : Promise<CallList>
-deleteCallList(id: string) : Promise<Boolean>
+editCallList(id: number, title:string) : Promise<CallList>
+deleteCallList(id: number) : Promise<Boolean>
 //addContact(callListId : CallList["id"]) : Promise<CallList>
 }
